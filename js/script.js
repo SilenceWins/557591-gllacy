b5dc23ec7ae549ca64638905cabe02dd28010019
SilenceWins <budozzer@gmail.com>
2021-04-29 18:26:22 +0300

    Карта, исправлен ошибочный backgr в каталоге

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
-var body = document.querySelector("body");
+var body = document.querySelector("body.main-body");
 var sliderRadios = document.querySelectorAll(".slider-radio");
 var sliderItems = document.querySelectorAll(".slider-item");
 var feedbackButton = document.querySelector(".main-map .feedback .btn");
 var modalFeedback = document.querySelector(".modal-feedback");
 var modalClose = modalFeedback.querySelector(".modal-close");
+var modalOverlay = document.querySelector(".modal-overlay");
 var backgrounds = ["#849D8F", "#8996A6", "#9D8B84"];
 
 
 var changeBackground = (index) => {
   body.style.backgroundColor = backgrounds[index];
   body.style.backgroundImage = "url('img/Slider-" + index + ".png')";
+}
+
+var closingFunc = (arrOfElements) => {
+  arrOfElements.forEach((elem) => {
+    elem.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      modalFeedback.classList.add("visually-hidden");
+    });
+  });
 }
 
 sliderRadios.forEach((sliderRadio, i) => {
@@ -35,10 +45,6 @@ feedbackButton.addEventListener("click", (e) => {
   modalFeedback.classList.remove("visually-hidden");
 });
 
-modalClose.addEventListener("click", (e) => {
-  e.preventDefault();
-  modalFeedback.classList.add("visually-hidden");
-});
-
+closingFunc([modalClose, modalOverlay]);
 
 
